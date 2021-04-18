1500342e22257c18b1571a4fbaef3e317aa66ac0
dpasse <dpasse@mpirik.com>
2021-04-18 12:00:41 -0500

    bit of clean up

--- src/eeyore/models/context.py
+++ src/eeyore/models/context.py
@@ -2,8 +2,8 @@ from typing import Dict, List
 
 
 class Context():
-    def __init__(self, sentence: str, tokens: List[str], **kwargs):
-        self.__sentence = sentence
+    def __init__(self, text: str, tokens: List[str], **kwargs):
+        self.__text = text
         self.__expected_length = len(tokens)
         self.__container: Dict[str, List[str]] = {
             'tokens': tokens
@@ -14,7 +14,7 @@ class Context():
 
     @property
     def sentence(self) -> str:
-        return self.__sentence
+        return self.__text
 
     @property
     def keys(self) -> List[str]:
@@ -22,7 +22,9 @@ class Context():
 
     def get(self, key: str) -> List[str]:
         if not key in self.__container:
-            raise KeyError()
+            raise KeyError(
+                f'{key} not found.'
+            )
 
         return self.__container[key]
 
@@ -30,8 +32,8 @@ class Context():
         if self.__expected_length != len(tags):
             raise ValueError(
                 f'for key == {key}, ' +
-                f'tags must have a length of {self.__expected_length}, ' + \
-                f'but was {len(tags)}'
+                f'tags must have a length of {self.__expected_length}, ' +
+                f'but was {len(tags)}.'
             )
 
         self.__container[key] = tags
--- tests/models/test_context.py
+++ tests/models/test_context.py
@@ -9,10 +9,13 @@ def test_chuker_single_term_phrase():
     context = Context(
         'We are not going to New York.',
         ['We', 'are', 'not', 'going', 'to', 'New', 'York', '.'],
-        **{
-            'entities': ['', '', '', '', '', 'LOC', 'LOC', '']
-        }
+        entities=['', '', '', '', '', 'LOC', 'LOC', ''],
+        negatives=['', '', 'NEG', '', '', '', '', ''],
+        negative_scope=['', '', 'NEG', 'NEG', 'NEG', 'NEG', 'NEG', 'NEG']
     )
 
+    assert len(context.keys) == 4
     assert len(context.get('tokens')) == 8
     assert len(context.get('entities')) == 8
+    assert len(context.get('negatives')) == 8
+    assert len(context.get('negative_scope')) == 8
