caf6ba02eb9eecc69537b67ca6e0dca565b528f6
dpasse <dpasse@mpirik.com>
2021-04-18 10:56:33 -0500

    start static tag and scope collections, pylint

---
+++ tests/rulesets/test_loader.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('src'))
+
+from eeyore.rulesets import load_tags, load_scopes
+from eeyore.rulesets.collection import available_tag_keys, available_scope_keys
+
+def test_load_tags():
+    tags = list(load_tags(available_tag_keys))
+    assert len(tags) == 5
+
+def test_load_scopes():
+    scopes = list(load_scopes(available_scope_keys))
+    assert len(scopes) == 1
