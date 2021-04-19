f53bf593f9ffd725854c5f30b1947e835f534fa2
dpasse <dpasse@mpirik.com>
2021-04-19 06:01:33 -0500

    generics, better typing

--- src/eeyore/models/__init__.py
+++ src/eeyore/models/__init__.py
@@ -2,3 +2,4 @@ from .phrases import RegexPhrase
 from .tag import Tag
 from .scope import Scope, ScopeDirection
 from .context import Context
+from .relationship import Relationship
--- src/eeyore/models/relationship.py
+++ src/eeyore/models/relationship.py
@@ -1,23 +1,26 @@
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, TypeVar
 
+
+T1 = TypeVar('T1')
+T2 = TypeVar('T2')
 
 class Relationship():
     def __init__(self,
-                 item1: Any,
-                 item2: Any,
+                 item1: T1,
+                 item2: T2,
                  attributes: Optional[Dict[str, Any]]):
         self.__item1 = item1
         self.__item2 = item2
         self.__attributes = attributes
 
     @property
-    def primary(self) -> Any:
+    def primary(self) -> T1:
         return self.__item1
 
     @property
-    def secondary(self) -> Any:
+    def secondary(self) -> T2:
         return self.__item2
 
     @property
-    def attributes(self) -> Any:
+    def attributes(self) -> Optional[Dict[str, Any]]:
         return self.__attributes
