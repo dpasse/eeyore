01282a8961267c1ca5eb043c41ef93216393d04d
dpasse <dpasse@mpirik.com>
2021-05-12 17:47:44 -0500

    adding pumps to pump data in

--- tests/integrations/test_spacy.py
+++ tests/integrations/test_spacy.py
@@ -7,17 +7,13 @@ from eeyore_nlp.models.context import Context
 from eeyore_nlp.integrations import SpacyIntegration
 
 def test_integration():
-
     integration = SpacyIntegration('en_core_web_sm')
-    doc = integration.loads(
+    doc = integration.load(
       Context(
         'I am hungry!',
         ['I', 'am', 'hungry', '!']
       )
     )
 
-    print(doc)
-
-    #context = integration.from(doc)
-
-    #assert context.get('tokens') == ['I', 'am', 'hungry', '!']
+    context = integration.dump(doc)
+    assert context.get('tokens') == ['I', 'am', 'hungry', '!']
